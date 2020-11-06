b4e5b09efc45c6551bcae3360f87624f431beb5d
mickizalan <71698357+mickizalan@users.noreply.github.com>
2020-11-06 09:20:56 +0100

    Add files via upload

---
+++ f0005b.py
@@ -0,0 +1,7 @@
+gondolt_szám = 56
+tipp = input('Kérlek add meg a jelszót. ')
+tipp = int(tipp)
+if gondolt_szám == tipp: # a gondolt szám egyenlő a tippel?
+    print('Helyes jeszó.')
+else: #különben
+    print('hozzáférés megtagadva.')
